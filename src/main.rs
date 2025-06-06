4752c558eedf384b2a26a994079d0bc7c94a7522
dpavam <hhz161@qmul.ac.uk>
2025-06-06 16:06:47 +0100

    feat: main optimisation changes

--- src/main.rs
+++ src/main.rs
@@ -32,27 +32,54 @@ struct Cli {
 // v=0.1.1
 // This should treat the vcf as a text file and not a vcf
 fn main() -> std::io::Result<()> {
-
     // Start the timer
     let start = Instant::now();
-    
+
+    // Parse cli arguments
     let args = Cli::parse();
 
-    // Read the file
-    let mut reader = VCFReader::new(BufReader::new(MultiGzDecoder::new(File::open(args.input)?)))?;
-    
-    // Create a writer
-    let mut writer = VCFWriter::new(File::create(args.output)?, &reader.header().clone())?;
+    // Let's parse the file with flate2 (compression) and BufReader (reading files)
+    let vcf = File::open(args.input)?;
+    let decoder = MultiGzDecoder::new(vcf);
+    let mut reader = BufReader::new(decoder);
 
-    // Create an empty record to read
-    let mut vcf_record = reader.empty_record();
+    //Prepare a writer for output file
+    let output_file = File::create(args.output)?;
+    let encoder = GzEncoder::new(output_file, Compression::fast());
+    let mut writer = BufWriter::new(encoder);
 
-    // Loop over the reader and write everything except the other fields in the INFO column
-    while reader.next_record(&mut vcf_record)? {
+    // Read lines and skip those with a #
+    let mut line = String::new();
+    // Read lines and skip those with a #
+    while reader.read_line(&mut line)? > 0 {
+        if line.starts_with('#') { 
+            writeln!(writer, "{}", line.trim_end())?;
+            // println!("{}", line.trim_end());
+        } else {
 
-        vcf_record.info.clear();
-        writer.write_record(&vcf_record)?;
+        // Split line by tabs
+        // TODO: instead of wiriting this to a vector, let's use string splicing
+        let mut fields: Vec<&str> = line.trim_end().split('\t').collect();
+
+        // let info_byte
+
+        // let begining = &line[0..]
+        
+        // When at index 7 (INFO), replace by a dot
+        if fields.len() > 7 {
+            fields[7] = ".";
+        }
+
+         // Reconstruct the line
+        let modified_line = fields.join("\t");
+        
+        writeln!(writer, "{}", &modified_line)?;
+        // println!("{}", &modified_line);
+        }
+       
+        line.clear();
     }
+
     let duration = start.elapsed();
     println!("Execution time: {:.2?}", duration);
     Ok(())
